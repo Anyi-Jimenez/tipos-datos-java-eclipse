b3a74ecfaea32796fe07d6ab055258f0987ff3a8
Anyi Jimenez <carbono12.011@gmail.com>
2023-06-13 15:17:42 -0600

    Se sube el ejercicio de Tipos de Variables en JAVA y Eclipse

---
+++ hotelVariable/src/hotelVariable/Ejercicios.java
@@ -0,0 +1,31 @@
+package hotelVariable;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte pisos = 16;
+		System.out.println(pisos);
+		
+		short habitaciones = 1600;
+		System.out.println(habitaciones);
+		
+		int huspedes = 6400;
+		System.out.println(huspedes);
+		
+		long reservaciones = 313600;
+		System.out.println(reservaciones);
+		
+        float costo = 150.80f;
+    	System.out.println(costo);
+    	
+    	double ganancia = 47290880.80d;
+    	System.out.println(ganancia);
+
+
+	}
+
+}
+
+
---
+++ tiposDeDatosYVariables/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author carbo
+ *
+ */
+module tiposDeDatosYVariables {
+}
